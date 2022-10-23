750051461d1eb794974ef47e9c4e0a91db2c545d
Laura llorente <lauesfltchr@gmail.com>
2022-10-23 18:03:25 +0200

    Add files for bot and prueba de audios

---
+++ bot/src/ConectApi/config.py
@@ -0,0 +1,5 @@
+URL_API = "http://backend/api/v1/state_users/"
+USER_BY_ID = "user_id/"
+USER_BY_USERNAME = "username/"
+STATE_USER_BY_ID = "state/" + USER_BY_ID
+STATE_USER_BY_USERNAME = "state/" + USER_BY_USERNAME
